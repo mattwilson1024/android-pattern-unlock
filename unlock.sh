7c7e66bede440bd9a66be216090ffedb64553a93
Matt Wilson <matt@matt-wilson.org>
2015-04-26 22:33:11 +0100

    Convert tabs to spaces for better rendering on GitHub

--- unlock.sh
+++ unlock.sh
@@ -30,25 +30,25 @@
 # 4 5 6
 # 7 8 9
 
-PATTERN="1 2 3 6 5 4 7 8 9"	# The unlock pattern to draw, space seperated
+PATTERN="1 2 3 6 5 4 7 8 9" # The unlock pattern to draw, space seperated
 
-COL_1=200					# X coordinate of column 1 (in pixels)
-COL_2=450					# X coordinate of column 2 (in pixels)
-COL_3=600					# X coordinate of column 3 (in pixels)
+COL_1=200                   # X coordinate of column 1 (in pixels)
+COL_2=450                   # X coordinate of column 2 (in pixels)
+COL_3=600                   # X coordinate of column 3 (in pixels)
 				
-ROW_1=600					# Y coordinate of row 1 (in pixels)
-ROW_2=800					# Y coordinate of row 2 (in pixels)
-ROW_3=1000					# Y coordinate of row 3 (in pixels)
+ROW_1=600                   # Y coordinate of row 1 (in pixels)
+ROW_2=800                   # Y coordinate of row 2 (in pixels)
+ROW_3=1000                  # Y coordinate of row 3 (in pixels)
 
-MULTIPLIER=2 				# Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
-							# Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
+MULTIPLIER=2                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
+                            # Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
 
-WAKE_SCREEN_ENABLED=true	# If true, the script will start by sending the power button press event
+WAKE_SCREEN_ENABLED=true    # If true, the script will start by sending the power button press event
 
-SWIPE_UP_ENABLED=true		# If true, the script will swipe upwards before drawing the pattern (e.g. for lollipop lockscreen)
-SWIPE_UP_X=450				# X coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
-SWIPE_UP_Y_FROM=1000		# Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
-SWIPE_UP_Y_TO=200			# End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_ENABLED=true       # If true, the script will swipe upwards before drawing the pattern (e.g. for lollipop lockscreen)
+SWIPE_UP_X=450              # X coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_Y_FROM=1000        # Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_Y_TO=200           # End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
 
 # =======================================================================================================================
 
